b71a225f0c7e3bd26b28e292ff823b7ed7ead308
Viktoriia <pecourses>
2023-11-19 17:54:46 +0200

    Add create naming contest section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -55,6 +55,29 @@ const Home = props => {
                 </Link>
               </div>
             </div>
+            <div className={styles.namingContestContainer}>
+              <section className={styles.namingContestSection}>
+                <div className={styles.namingContestInfo}>
+                  <div>
+                    <img src='https://www.squadhelp.com/html/html/static_images/h-icon1.svg' />
+                  </div>
+                  <div className={styles.namingContestText}>
+                    <h2 className={styles.namingContestHead}>
+                      Naming Contests
+                    </h2>
+                    <p className={styles.namingContestP}>
+                      Custom name suggestions from 100s of naming experts as you
+                      are guided through our naming agency-style process
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.namingContestLinkContainer}>
+                  <Link className={styles.namingContestLink} to='/startContest'>
+                    Get a Custom Name
+                  </Link>
+                </div>
+              </section>
+            </div>
             <div className={styles.greyContainer}>
               <SlideBar
                 images={carouselConstants.mainSliderImages}
